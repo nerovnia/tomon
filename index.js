9bf89dc93d63149fc4792189c6ac8955c2c41516
Volodymyr Nerovnia <nerv@i.ua>
2020-03-09 15:21:02 +0200

    Fix some easy mistakes.

--- index.js
+++ index.js
@@ -2,52 +2,24 @@ const mongoose = require("mongoose");
 const fs = require('fs');
 
 const fjsondbauth = "./dbauth.json";
+const fexport = "./export/export.json";
 
 const dbauth = JSON.parse(fs.readFileSync(fjsondbauth, 'utf8'));
+const exportModels = JSON.parse(fs.readFileSync(fexport, 'utf8'));
+mongoose.connect(dbauth.uri, {user: dbauth.login, pass: dbauth.password, useNewUrlParser: true,useUnifiedTopology:true, authSource: "admin"});
 
-console.log(dbauth.login, dbauth.password);
-
-
-//mongoose.connect("mongodb://root:example@localhost:27017/uz", {user:'root', pass:'example', useNewUrlParser: true,useUnifiedTopology:true, authSource: "admin"});
-mongoose.connect("mongodb://root:example@localhost:27017/uz", {user:dbauth.user, pass:dbauth.password, useNewUrlParser: true,useUnifiedTopology:true, authSource: "admin"});
-
-const Schema = mongoose.Schema;
-const cowokerScheme = new Schema({
-      fname: String,
-      mname: String,
-      oname: String,
-      ws: String,
-      bd: String,
-      op: Number,
-      ph: [
-        String
-      ]
-});
-
-const cowokersScheme = new Schema({
-      cowokers: [cowokerScheme]
-});
-
-const Cowokers = mongoose.model("Cowokers", cowokersScheme);
-
-fs.readFile("./export/ph.json", (err, data) => {
-  if (err) throw err;
-  let obj = JSON.parse(data);
-  //console.log(obj);
-  cowokers = new Cowokers();
-  cowokers.cowokers = obj.cowokers;
-  cowokers.save(function(err){
-    mongoose.disconnect();  // отключение от базы данных
-      
-    if(err) return console.log(err);
-    console.log("Сохранен объект");
+exportModels.models.forEach(model => {
+  let dbmodel = require('./'+exportModels.ptomodel + model.model).Model();
+  fs.readFile(exportModels.ptodata + model.expfile + '.json', (err, data) => {
+    if (err) throw err;
+    let obj = JSON.parse(data);
+    dbmodel.objdata = obj.objdata;
+    dbmodel.save(function (err) {
+      mongoose.disconnect();
+      if (err) return console.log(err);
+      console.log('Export data is complite!');
+    });
   });
-
-
-
-
 });
-
-  
 
 
